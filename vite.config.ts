61a660ae2294a2c8be3263dafe08bb85b813a340
Elga J H <henryelga2004@gmail.com>
2025-09-01 17:44:06 +0100

    CHanges for publishing to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dundalkpresbyterianchurch/",
+  base:
+    process.env.NODE_ENV === "production" ? "/dundalkpresbyterianchurch/" : "/",
   plugins: [react(), tailwindcss()],
 });
