dfcd7ee6eb33188d85e8c59a64a47a38981b2a9e
Elga J Henry <henryelga2004@gmail.com>
2026-09-16 15:29:55 +0100

    Fix base path for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/dundalkpresbyterianchurch/",
+  base: "/",
   plugins: [react(), tailwindcss()],
 });
